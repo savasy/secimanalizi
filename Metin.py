4b41c631b3e486f51af318c856f068120a34b70f
savas <savasy@gmail.com>
2015-10-09 15:26:08 +0300

    I changed Metin.py file

--- Metin.py
+++ Metin.py
@@ -1,30 +1,31 @@
+import os, csv, nltk, sys, math
+
 class Metin:
  'Verilen Herhangi bir metin, yazı, haber vesaire'
- def __init__(self, text, stop, nplist):
+ def __init__(self, afile, stop=[], nplist=[]):
   'stop: stop word list'
-  'nplist: kelime obekleri noun phrase olabilir, istanbul ili   turkiye cumhuriyeti gibi '
-  self.text=text
+  'nplist: kelime obekleri noun phrase olabilir, ["istanbul ili"  , "turkiye cumhuriyeti gibi"]'
   self.stop=stop
   self.nplist=nplist
-  self.di()
- def display(self):
-  print("hello")
+  self.text=self.loadfile(afile)
+  self.elim()
+ def loadfile(self,afile):
+  text=[]
+  with open(afile) as f:
+   for line in f:
+    line=self.npbuild(line.strip().lower()) 
+    text =text+ line.strip().split() 
+  return text 
+ def elim(self): 
+  self.text= [w for w in self.text if w not in self.stop]
+ def npbuild(self, aline):
+  for np in self.nplist:
+   if(np in aline):
+    print(np,aline) 
+    newnp=np.replace(" ","_") 
+    aline=aline.replace(np, newnp)
+  return aline
 
- def eliminate(self):
-  'eliminate stop words'
-  print("hmm")
-
- def repNp(self):
-  'make all phrases in nplist adjacent e.g. new_york ' 
-  print("hmm")
- def fd(self):
-  ' return frequency dist of term'
- def terms(self):
-  'return only terms'
-
-
-
-
-
-
+  
+  
  
